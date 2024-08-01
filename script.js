5b569643e75078f62872de4b0a2f86704a4ce90d
Parveen <155971721+Parveen082@users.noreply.github.com>
2024-08-01 20:40:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add event listener to toggle button
+document.querySelector('.navbar-toggler').addEventListener('click', function() {
+    // Toggle navbar nav
+    document.querySelector('.navbar-nav').classList.toggle('show');
+  });
